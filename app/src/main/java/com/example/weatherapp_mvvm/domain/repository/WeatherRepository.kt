76bdb4d210940099309f6dff80b37eed4d340620
chayan <chayanchowdhury@gmail.com>
2024-06-22 22:53:40 +0530

    foundation level changes

--- app/src/main/java/com/example/weatherapp_mvvm/domain/repository/WeatherRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.weatherapp_mvvm.domain.repository
-
-import com.example.weatherapp_mvvm.domain.model.Weather
-
-
-interface WeatherRepository {
-    suspend fun getWeather(): List<Weather>
-}
